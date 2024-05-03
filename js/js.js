bf388bc8b99e2d07de89f80e44dc19e1988456c9
pauloborgesgh <79487337+pauloborgesgh@users.noreply.github.com>
2024-05-03 17:04:45 -0300

    Add files via upload
    Chat em tempo Real

---
+++ js/js.js
@@ -0,0 +1,33 @@
+const socket = io('http://pauloborgesgh.github.io'); // Substitua localhost pelo IP do seu servidor
+
+const messagesContainer = document.getElementById('messages');
+const inputMsg = document.getElementById('input-msg');
+const sendBtn = document.getElementById('send-btn');
+
+
+function addMessage(message) {
+    const div = document.createElement('div');
+    div.textContent = message;
+    messagesContainer.appendChild(div);
+}
+
+s
+sendBtn.addEventListener('click', () => {
+    const message = inputMsg.value;
+    if (message.trim() !== '') {
+        socket.emit('chatMessage', message);
+        inputMsg.value = '';
+        addMessage('Você: ' + message); 
+    }
+});
+
+inputMsg.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        sendBtn.click();
+    }
+});
+
+
+socket.on('message', (message) => {
+    addMessage('Outro Usuário: ' + message);
+});
